e42c72da372a316da789b64aba858b888ae59a5d
Vinzenz Branzk <vinz@vbspace.de>
2023-03-27 22:59:14 +0200

    Add loading spinner

--- frontend/src/app/shared/modules/assistance-plan-analysis/assistance-plan-analysis.component.ts
+++ frontend/src/app/shared/modules/assistance-plan-analysis/assistance-plan-analysis.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {AssistancePlanService} from "../../../services/assistance-plan.service";
 import {AssistancePlanEvaluation} from "../../../dtos/assistance-plan-evaluation.model";
+import {delay} from "rxjs";
 
 @Component({
   selector: 'app-assistance-plan-analysis',
@@ -13,15 +14,28 @@ export class AssistancePlanAnalysisComponent implements OnInit {
 
   eval: AssistancePlanEvaluation = new AssistancePlanEvaluation()
 
+  // STATES
+  isSubmitting = false;
+
   constructor(
     private assistancePlanService: AssistancePlanService
   ) { }
 
   ngOnInit(): void {
+    this.isSubmitting = true
     this.loadAnalysis()
   }
 
   loadAnalysis() {
-    this.assistancePlanService.getEvaluationById(this.id).subscribe((value) => this.eval = value)
+    this.assistancePlanService
+      .getEvaluationById(this.id)
+      .subscribe({
+        next: (value) => {
+          this.eval = value
+          this.isSubmitting = false
+        },
+        error: () => this.isSubmitting = false,
+        complete: () => this.isSubmitting = false
+      })
   }
 }
