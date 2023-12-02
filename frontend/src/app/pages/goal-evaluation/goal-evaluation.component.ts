10141669016b472cbe2095f22f611bcc218d0877
Vinzenz Branzk <vinz@vbspace.de>
2023-12-02 13:52:28 +0100

    Add tab selection over url

--- frontend/src/app/pages/goal-evaluation/goal-evaluation.component.ts
+++ frontend/src/app/pages/goal-evaluation/goal-evaluation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {AssistancePlanService} from "../../services/assistance-plan.service";
 
 @Component({
   selector: 'app-goal-evaluation',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalEvaluationComponent implements OnInit {
 
-  constructor() { }
+  // CONST
+  private validTabIndices = [0,1,2];
+  private tabParamName = 'tab';
+  private idParamName = 'id';
+
+  // VAR
+  assistancePlanId = 0;
+  tabIndex = 0;
+
+  constructor(private assistancePlanService: AssistancePlanService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.executeURLParams();
+    this.assistancePlanService.getById(this.assistancePlanId).subscribe({
+      next: value => console.log(value)
+    })
+  }
+
+  executeURLParams() {
+    this.route.params.subscribe(params => {
+      // tab
+      if (params[this.tabParamName]) {
+        const urlTabIndex = +params[this.tabParamName]
+        if (this.validTabIndices.includes(urlTabIndex, 0)) {
+          this.tabIndex = urlTabIndex;
+        }
+      }
+
+      // goalId
+      if (params[this.idParamName]) {
+        this.assistancePlanId = +params[this.idParamName];
+      }
+    });
   }
 
 }
