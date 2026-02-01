34850208b0d3a8ac0fccfabc4712a360895e9676
Vinzenz Branzk <vinz@vbspace.de>
2026-02-01 18:13:09 +0100

    Remove log warning

--- backend/src/main/kotlin/de/vinz/openfls/security/SecurityConfiguration.kt
+++ backend/src/main/kotlin/de/vinz/openfls/security/SecurityConfiguration.kt
@@ -13,9 +13,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -53,16 +52,6 @@ class SecurityConfiguration {
     private val corsAllowedOriginPatterns: List<String> = listOf()
 
     @Bean
-    fun authenticationProvider(
-        userDetailsService: UserDetailsService,
-        passwordEncoder: PasswordEncoder
-    ): DaoAuthenticationProvider {
-        return DaoAuthenticationProvider(userDetailsService).apply {
-            setPasswordEncoder(passwordEncoder)
-        }
-    }
-
-    @Bean
     fun modelMapper(): ModelMapper? = ModelMapper()
 
     @Bean
@@ -70,8 +59,14 @@ class SecurityConfiguration {
 
     @Bean
     @Throws(Exception::class)
-    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
-        return authenticationConfiguration.authenticationManager
+    fun authenticationManager(
+        http: HttpSecurity,
+        userDetailsService: UserDetailsService,
+        passwordEncoder: PasswordEncoder
+    ): AuthenticationManager {
+        val builder = http.getSharedObject(AuthenticationManagerBuilder::class.java)
+        builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder)
+        return builder.build()
     }
 
     @Bean
