862edbac36d512f45d101212b8e2ea8ccbf57a5b
Vinzenz Branzk <vinz@vbspace.de>
2026-02-14 07:25:38 +0100

    Add assistance plan create dto

---
+++ frontend/src/app/shared/dtos/assistance-plan-create-dto.model.ts
@@ -0,0 +1,23 @@
+
+export class AssistancePlanCreateDto {
+  start: string = Date.now().toLocaleString();
+  end: string = Date.now().toLocaleString();
+  clientId: number = 0;
+  institutionId: number = 0;
+  sponsorId: number = 0;
+  hours: AssistancePlanCreateHourDto[] = [];
+  goals: AssistancePlanCreateGoalDto[] = [];
+}
+
+export class AssistancePlanCreateHourDto {
+  weeklyHours: number = 0.0;
+  hourTypeId: number = 0;
+}
+
+export class AssistancePlanCreateGoalDto {
+  title: string = "";
+  description: string = "";
+  assistancePlanId: number = 0;
+  institutionId: number | null = null;
+  hours: AssistancePlanCreateHourDto[] = [];
+}
